9953f644e7bef7d1cd0ccce3cff6df9c11c2ddad
Anderson <anderson@sinprosp.org.br>
2019-09-04 14:53:38 -0300

    Projeto refatorado

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@
 /**
  * rota home
  */
-Route::name('home')->get('/', 'Website\HomeController@index');
+Route::name('home')->get('/', function(){
+    return redirect('login');
+});
 
 /**
  * Corrige erro: se estiver logado e acessar rota '/login' estava redirecionando para '/home'
@@ -34,40 +36,6 @@ Route::post('/login', 'Auth\LoginController@login');
  * logout
  */
 Route::name('logout')->post('/logout', 'Auth\LoginController@logout');
-
-/**
- * rota noticias
- */
-Route::resource('noticias', 'Website\NoticiasController', ['only' => ['index', 'show']]);
-
-/**
- * rota fonoaudiologia
- */
-Route::resource('fono', 'Website\FonoAudiologiaController', ['only' => ['index']]);
-
-/**
- * atendimento eletrônico
- */
-Route::resource('atendimento-eletronico', 'Website\AtendimentoEletronicoController');
-
-/**
- * páginas principais
- */
-Route::name('paginas-principais')->get('/{url_pagina}', 'Website\PaginasPrincipaisController@show', ['only' => ['show']]);
-
-/**
- * rotas para download do pdf
- */
-Route::name('convencao.asset')->get('/pdf/{convencao}', 'Website\ConvencoesController@convencaoWebAsset');
-Route::name('aditamento.asset')->get('/pdf-aditamento/{convencao}', 'Website\ConvencoesController@aditamentoWebAsset');
-/***
- * Convençoes e acordo
- */
-Route::group(['prefix' => 'convencoes-e-acordo', 'as' => 'convencao.', 'namespace' => 'Website'], function(){
-    Route::name('index')->get('{convencoes_entidade}', 'ConvencoesController@index');
-    Route::name('show')->get('{convencoes_entidade}/{convencao}', 'ConvencoesController@show');
-    Route::name('clausulas.show')->get('{convencoes_entidade}/{convencao}/{convencao_clausula}', 'ClausulasController@show');
-});
 
 /**
  * Rota administração usuário
