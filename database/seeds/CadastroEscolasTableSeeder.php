a2d560d22d1fcf0e7388ccc186956286100a0106
Anderson <ander-reis@hotmail.com>
2019-10-16 13:20:07 -0300

    Correção cidade migration Cadastro Escolas

--- database/seeds/CadastroEscolasTableSeeder.php
+++ database/seeds/CadastroEscolasTableSeeder.php
@@ -11,6 +11,17 @@ class CadastroEscolasTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(\Cadastro\Models\CadastroEscolas::class)->create();
+        factory(\Cadastro\Models\CadastroEscolas::class)->create(['CEP' => '01000-000']);
+        factory(\Cadastro\Models\CadastroEscolas::class)->create(['CEP' => '01001-000']);
+        factory(\Cadastro\Models\CadastroEscolas::class)->create(['CEP' => '01999-999']);
+        factory(\Cadastro\Models\CadastroEscolas::class)->create(['CEP' => '02000-000']);
+        factory(\Cadastro\Models\CadastroEscolas::class)->create(['CEP' => '02001-000']);
+        factory(\Cadastro\Models\CadastroEscolas::class)->create(['CEP' => '02999-999']);
+        factory(\Cadastro\Models\CadastroEscolas::class)->create(['CEP' => '04000-000']);
+        factory(\Cadastro\Models\CadastroEscolas::class)->create(['CEP' => '04001-000']);
+        factory(\Cadastro\Models\CadastroEscolas::class)->create(['CEP' => '04999-999']);
+        factory(\Cadastro\Models\CadastroEscolas::class)->create(['CEP' => '05600-000']);
+        factory(\Cadastro\Models\CadastroEscolas::class)->create(['CEP' => '05601-000']);
+        factory(\Cadastro\Models\CadastroEscolas::class)->create(['CEP' => '05999-999']);
     }
 }
